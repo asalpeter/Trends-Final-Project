61714e54f49327afc0dd86c0be329245780270f8
ZainHBilal28 <zb98@cornell.edu>
2024-12-03 21:05:27 -0500

    removing leak

--- Final/frontend/src/components/auth.ts
+++ Final/frontend/src/components/auth.ts
@@ -2,38 +2,36 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { signInWithPopup, signOut } from "firebase/auth";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyC0aQKF-IhUgKo2N1_c_5JRxOiaghABXoU",
-    authDomain: "guessing-game-76f46.firebaseapp.com",
-    projectId: "guessing-game-76f46",
-    storageBucket: "guessing-game-76f46.firebasestorage.app",
-    messagingSenderId: "1018694836761",
-    appId: "1:1018694836761:web:8a1a06c71931acb759c088",
-    measurementId: "G-430WT51PK1"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
-  export const signInWithGoogle = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      return result.user; // Returns user info
-    } catch (error) {
-      console.error("Error signing in");
-    }
-  };
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    return result.user; // Returns user info
+  } catch (error) {
+    console.error("Error signing in");
+  }
+};
 
-  export const logOut = async () => {
-    try {
-      await signOut(auth);
-      console.log("User signed out.");
-    } catch (error) {
-      console.error("Error signing out");
-    }
-  };
-  
-  const clientApp = initializeApp(firebaseConfig);
-  const auth = getAuth(clientApp);
-  const provider = new GoogleAuthProvider();
+export const logOut = async () => {
+  try {
+    await signOut(auth);
+    console.log("User signed out.");
+  } catch (error) {
+    console.error("Error signing out");
+  }
+};
 
-  export { auth, provider };
-  
+const clientApp = initializeApp(firebaseConfig);
+const auth = getAuth(clientApp);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
